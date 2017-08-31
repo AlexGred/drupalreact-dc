cdac269eba1e22172b7946c9a7dea35be010f048
Alex Dolzhenkov <alexgred,dev@gmail.com>
2017-08-31 23:48:25 +0300

    Added uikit. Users page server data

--- src/containers/users.js
+++ src/containers/users.js
@@ -5,19 +5,23 @@ export default class Users extends Component {
   render() {
     let data = this.props.users;
 
+    console.log(data);
+
     var userData = data.map((item, index) => {
       let img = 'http://dev.drupal-coder.ru' + decodeURIComponent(item.user_picture);
 
       return (
-        <div className='user-item' key={index}>
-          <div>{item.uid}</div>
+        <div className='item-user uk-width-1-2 uk-width-1-3@s uk-width-1-4@m' key={index}>
           <img className='img' src={img} />
+          <div className='name'>{item.field_surname}</div>
+          <div className='post'>{item.field_post}</div>
+          <div className='hired'>{item.field_hired}</div>
         </div>
       );
     });
 
     return (
-      <div>
+      <div className='uk-grid'>
         {userData}
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/app';
+import 'jquery';
 import './styles/_style.less';
+import 'uikit';
+import 'uikit/dist/css/uikit.min.css';
 
 
 render (
