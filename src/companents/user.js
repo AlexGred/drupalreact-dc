ac92c2996c1e19763f71634371a19c1e361aaa4a
Alex Dolzhenkov <alexgred.dev@gmail.com>
2017-09-07 21:52:11 +0300

    User page info and fields

--- src/companents/user.js
+++ src/companents/user.js
@@ -23,10 +23,50 @@ export default class User extends Component {
 
   render() {
     let user = this.state.user;
+    let img = 'http://dev.drupal-coder.ru' + decodeURIComponent(user.user_picture);
 
     return (
-      <div className='uk-grid'>
-        <div className='uk-width-1-1'>Раздел {user.name}</div>
+      <div>
+        <div className='uk-grid'>
+          <div className='uk-width-1-3 wrap-img'>
+            <img src={img} />
+          </div>
+          <div className='uk-width-2-3 wrap-text'>
+            <div className='field-name'>
+              <div className='text'>
+                {user.name}
+              </div>
+            </div>
+            <div className='field-post'>
+              <div className='text'>
+                {user.field_post}
+              </div>
+            </div>
+            <div className='field-hired'>
+              <div className='label'>
+                
+              </div>
+              <div className='text'>
+                {user.field_hired}
+              </div>
+            </div>
+            <div className='field-obrazovanie'>
+              <div className='label'>
+                Образование:
+              </div>
+              <div className='text'>
+                {user.field_obrazovanie}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='uk-grid'>
+          <div className='uk-width-1-1 wrap-description'>
+            <div className='field-about'>
+              {user.field_o_sebe}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
